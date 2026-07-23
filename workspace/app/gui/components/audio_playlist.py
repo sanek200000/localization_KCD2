a33c634a5e331d5b7d6a03818dc5695d0bd478b9
heist <heist2000@gmail.com>
2026-07-23 14:13:48 +0300

    feat(gui): add sequential audio playlist component for automatic subtitle voice playback

---
+++ workspace/app/gui/components/audio_playlist.py
@@ -0,0 +1,22 @@
+from nicegui import ui
+
+
+class AudioPlaylist:
+    def __init__(self) -> None:
+        self._players: list[ui.audio] = list()
+
+    def add(self, player: ui.audio):
+        self._players.append(player)
+
+    def start(self):
+        if self._players:
+            self._play(0)
+        return
+
+    def _play(self, index: int):
+        if index >= len(self._players):
+            return
+
+        player = self._players[index]
+        player.run_method("play")
+        player.on("ended", lambda _, i=index + 1: self._play(i))
